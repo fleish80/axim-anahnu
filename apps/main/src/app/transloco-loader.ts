c8f7e5439a1dd24d1e357fb1e5a0f90509be9f3e
fleish80 <Fleish.D@gmail.com>
2023-10-18 22:01:39 +0300

    transloco-lib

--- apps/main/src/app/transloco-loader.ts
+++ apps/main/src/app/transloco-loader.ts
@@ -1,14 +1,16 @@
-import { HttpClient } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
-import { ENVIRONMENT } from "@axim-anahnu/common/environment";
-import { Translation, TranslocoLoader } from "@ngneat/transloco";
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ENVIRONMENT } from '@axim-anahnu/common/environment';
+import { Translation, TranslocoLoader } from '@ngneat/transloco';
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
-    #http = inject(HttpClient);
-    #environment = inject(ENVIRONMENT);
+  #http = inject(HttpClient);
+  #environment = inject(ENVIRONMENT);
 
-    getTranslation(lang: string) {
-        return this.#http.get<Translation>(`${this.#environment.baseUrl}/assets/i18n/${lang}.json`);   
-    }
+  getTranslation(lang: string) {
+    return this.#http.get<Translation>(
+      `${this.#environment.baseUrl}/assets/i18n/${lang}.json`
+    );
+  }
 }
