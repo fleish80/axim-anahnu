afc7e694f2bbd68248462e25c6d8acf72ed1732d
fleish80 <Fleish.D@gmail.com>
2023-10-26 09:22:19 +0300

    deploy

---
+++ apps/main/src/environments/environment.firebase.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    firebase: {
+      projectId: 'axim-anahnu',
+      appId: '1:452498995887:web:b1c14736c80d0dc471325a',
+      storageBucket: 'axim-anahnu.appspot.com',
+      apiKey: 'AIzaSyDjo1NrWyYDtciIIdpU4tK1P4oyfqRSBuw',
+      authDomain: 'axim-anahnu.firebaseapp.com',
+      messagingSenderId: '452498995887',
+      measurementId: 'G-LXN0Q1WJNQ',
+    },
+    baseUrl: 'https://axim-anahnu.web.app/'
+  };
+  
