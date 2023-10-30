3efb1a8990961a4e2c2a0695d245d9db9f27bb92
fleish80 <Fleish.D@gmail.com>
2023-10-30 23:06:12 +0200

    add menu toolbar

--- libs/content/content/components/content/content.component.ts
+++ libs/content/content/components/content/content.component.ts
@@ -2,20 +2,34 @@ import { Component } from '@angular/core';
 import { DescriptionComponent } from '@axim-anahnu/content/description';
 import { HistoryComponent } from '@axim-anahnu/content/history';
 import { SourceComponent } from '@axim-anahnu/content/source';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @Component({
   selector: 'aa-content',
   standalone: true,
   template: `
+  <ng-container *transloco="let t; read 'content'">
     <mat-toolbar color="primary">
-      <span>בוא נראה את זה פה</span>
+      <div class="toolbar-buttons">
+        <button class="toolbar-button" mat-flat-button color="primary">{{t('description')}}</button>
+        <button class="toolbar-button"mat-flat-button color="primary">{{t('history')}}</button>
+        <button class="toolbar-button" mat-flat-button color="primary">{{t('information')}}</button>
+      </div>
     </mat-toolbar>
-  <aa-description/>
-  <aa-history/>
-  <aa-source>
+    <aa-description/>
+    <aa-history/>
+    <aa-source/>
+  </ng-container>
   `,
-  styles: [],
-  imports: [DescriptionComponent, HistoryComponent, SourceComponent, MatToolbarModule]
+  styles: [
+    `
+      .toolbar-button {
+        font-size: 30px;
+      }
+    `
+  ],
+  imports: [DescriptionComponent, HistoryComponent, SourceComponent, MatToolbarModule, MatButtonModule, TranslocoModule]
 })
 export class ContentComponent { }
