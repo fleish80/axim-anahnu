8781677abc80d8f387e06a29e7177f7c9943ba2a
fleish80 <Fleish.D@gmail.com>
2023-11-02 21:26:56 +0200

    article

--- libs/content/description/components/description/description.component.ts
+++ libs/content/description/components/description/description.component.ts
@@ -7,9 +7,25 @@ import { TranslocoModule } from '@ngneat/transloco';
   imports: [TranslocoModule],
   template: `
     <ng-container *transloco="let t; read 'content'">
-      {{t('hello')}}
+      <article class="article" color="primary">
+        <h2 class="article-title">{{t('article.title')}}</h2>
+        <p class="article-paragraph">{{t('article.paragraph.0')}}</p>
+        <p class="article-paragraph">{{t('article.paragraph.1')}}</p>
+        <p class="article-paragraph">{{t('article.paragraph.2')}}</p>
+        <p class="article-paragraph">{{t('article.paragraph.3')}}</p>
+        <p class="article-paragraph">{{t('article.paragraph.4')}}</p>
+        <p class="article-paragraph">{{t('article.paragraph.5')}}</p>
+        <p class="article-paragraph">{{t('article.paragraph.6')}}</p>
+        <p class="article-paragraph">{{t('article.paragraph.7')}}</p>
+        <ul class="article-paragraph">
+          <li>{{t('article.paragraph.7.list.0')}}</li>
+          <li>{{t('article.paragraph.7.list.1')}}</li>
+          <li>{{t('article.paragraph.7.list.2')}}</li>
+        </ul>
+        <p class="article-paragraph">{{t('article.paragraph.8')}}</p>
+      </article>
     </ng-container>
   `,
-  styles: [],
+  styleUrls: ['./description.component.scss']
 })
-export class DescriptionComponent {}
+export class DescriptionComponent { }
