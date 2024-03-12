eedbb44f6d207c2c00728754ebe529eafc93d21d
fleish80 <Fleish.D@gmail.com>
2024-03-12 13:22:15 +0200

    axim anahnu

--- libs/content/description/components/description/description.component.ts
+++ libs/content/description/components/description/description.component.ts
@@ -6,25 +6,23 @@ import { TranslocoModule } from '@ngneat/transloco';
   standalone: true,
   imports: [TranslocoModule],
   template: `
-    <ng-container *transloco="let t; read 'content'">
-      <article class="mat-elevation-z24 content article" color="primary">
-        <h2 class="article-title">{{t('article.title')}}</h2>
-        <p class="article-paragraph">{{t('article.paragraph.0')}}</p>
-        <p class="article-paragraph">{{t('article.paragraph.1')}}</p>
-        <p class="article-paragraph">{{t('article.paragraph.2')}}</p>
-        <p class="article-paragraph">{{t('article.paragraph.3')}}</p>
-        <p class="article-paragraph">{{t('article.paragraph.4')}}</p>
-        <p class="article-paragraph">{{t('article.paragraph.5')}}</p>
-        <p class="article-paragraph">{{t('article.paragraph.6')}}</p>
-        <p class="article-paragraph">{{t('article.paragraph.7')}}</p>
-        <ul class="article-paragraph">
-          <li>{{t('article.paragraph.7.list.0')}}</li>
-          <li>{{t('article.paragraph.7.list.1')}}</li>
-          <li>{{t('article.paragraph.7.list.2')}}</li>
-        </ul>
-        <p class="article-paragraph">{{t('article.paragraph.8')}}</p>
-      </article>
-    </ng-container>
+    <article class="mat-elevation-z24 content article" *transloco="let t; read 'content'">
+      <h2 class="article-title">{{ t('article.title') }}</h2>
+      <p class="article-paragraph">{{ t('article.paragraph.0') }}</p>
+      <p class="article-paragraph">{{ t('article.paragraph.1') }}</p>
+      <p class="article-paragraph">{{ t('article.paragraph.2') }}</p>
+      <p class="article-paragraph">{{ t('article.paragraph.3') }}</p>
+      <p class="article-paragraph">{{ t('article.paragraph.4') }}</p>
+      <p class="article-paragraph">{{ t('article.paragraph.5') }}</p>
+      <p class="article-paragraph">{{ t('article.paragraph.6') }}</p>
+      <p class="article-paragraph">{{ t('article.paragraph.7') }}</p>
+      <ul class="article-paragraph">
+        <li>{{ t('article.paragraph.7.list.0') }}</li>
+        <li>{{ t('article.paragraph.7.list.1') }}</li>
+        <li>{{ t('article.paragraph.7.list.2') }}</li>
+      </ul>
+      <p class="article-paragraph">{{ t('article.paragraph.8') }}</p>
+    </article>
   `,
   styleUrls: ['./description.component.scss']
 })
--- libs/content/history/components/history/history.component.ts
+++ libs/content/history/components/history/history.component.ts
@@ -3,22 +3,22 @@ import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
 import { SecondHouseDestructionComponent } from '../second-house-destruction/second-house-destruction.component';
 import { HolocaustComponent } from '../holocaust/holocaust.component';
+import { TranslocoDirective } from '@ngneat/transloco';
 
 @Component({
   selector: 'aa-history',
   standalone: true,
-  imports: [CommonModule, MatTabsModule, SecondHouseDestructionComponent, HolocaustComponent],
+  imports: [CommonModule, MatTabsModule, SecondHouseDestructionComponent, HolocaustComponent, TranslocoDirective],
   template: `
-
-    <mat-tab-group color="primary">
-      <mat-tab label="חורבן בית שני">
+    <mat-tab-group color="primary" *transloco="let t; read 'content'">
+      <mat-tab [label]="t('second-house-label')">
         @defer (on viewport) {
           <aa-second-house-destruction/>
         } @placeholder {
           <span>Loading...</span>
         }
       </mat-tab>
-      <mat-tab label="שואה">
+      <mat-tab [label]="t('holocaust-label')">
         @defer (on viewport) {
           <aa-holocaust/>
         } @placeholder {
