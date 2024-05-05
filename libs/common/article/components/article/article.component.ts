896460d172dbf5ca1c3474859e7f850599c9965a
fleish80 <Fleish.D@gmail.com>
2024-05-05 22:03:50 +0300

    axim anahnu

--- libs/common/article/components/article/article.component.ts
+++ libs/common/article/components/article/article.component.ts
@@ -1,12 +1,34 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { TranslocoDirective } from '@ngneat/transloco';
 
 @Component({
   selector: 'axim-anahnu-article',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>article works!</p>`,
+  imports: [
+    TranslocoDirective
+  ],
+  template: `
+  <article *transloco="let t; read 'content'">
+    @for (index of sizeArr(); track index) {
+      <p class="article-paragraph">{{ t('text.' + index) }}</p>
+    }
+  </article>
+
+  `,
   styleUrl: './article.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ArticleComponent {}
+export class ArticleComponent {
+
+  name = input.required<string>();
+  size = input.required<number>();
+
+  sizeArr = computed(() => {
+    const arr: number[] = []
+    for (let i = 0; i < this.size(); i++) {
+      arr.push(i);
+    }
+    return arr;
+  })
+
+}
