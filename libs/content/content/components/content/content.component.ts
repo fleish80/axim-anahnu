57211c4dea3648201ada653a414b1df70e251b87
fleish80 <Fleish.D@gmail.com>
2024-05-13 16:41:38 +0300

    axim anahnu

--- libs/content/content/components/content/content.component.ts
+++ libs/content/content/components/content/content.component.ts
@@ -1,40 +1,18 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { DescriptionComponent } from '@axim-anahnu/content/description';
 import { HistoryComponent } from '@axim-anahnu/content/history';
 import { SourceComponent } from '@axim-anahnu/content/source';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { TranslocoModule } from '@ngneat/transloco';
-import { YoutubeSourceService } from '../../../source/service/youtube-source/youtube-source.service';
 
 @Component({
   selector: 'aa-content',
   standalone: true,
   template: `
-  <ng-container *transloco="let t; read 'content'">
-    <mat-toolbar color="primary">
-      <div class="toolbar-buttons">
-        <button class="toolbar-button" mat-flat-button color="primary">{{t('description')}}</button>
-        <button class="toolbar-button" mat-flat-button color="primary">{{t('history')}}</button>
-        <button class="toolbar-button" mat-flat-button color="primary">{{t('information')}}</button>
-      </div>
-    </mat-toolbar>
     <aa-description/>
     <aa-history/>
     <aa-source/>
-  </ng-container>
   `,
-  styles: [
-    `
-      .toolbar-button {
-        font-size: 30px;
-      }
-    `
-  ],
-  imports: [DescriptionComponent, HistoryComponent, SourceComponent, MatToolbarModule, MatButtonModule, TranslocoModule]
+  imports: [DescriptionComponent, HistoryComponent, SourceComponent]
 })
 export class ContentComponent {
-
-  #content = inject(YoutubeSourceService);
 
 }
